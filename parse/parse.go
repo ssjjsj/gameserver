9482a7291ef340028714535999adf90cf3c9257a
ssjjsj <ssjjsj11@gmail.com>
2018-03-08 12:34:22 +0800

    add parse change

--- parse/parse.go
+++ parse/parse.go
@@ -1,5 +1,7 @@
 package parse
 
+import "encoding/binary"
+
 const (
 	WaitForLength = 0
 	WaitForData = 1
@@ -8,24 +10,40 @@ const (
 type Parse struct
 {
 	curData []byte
+	curHeadLength int
 	curDataLength int
 	curStatus int
 	needDataLength int
 }
 
 
-func (p Parse) parse(data []byte, num int) {
+func (p Parse) parse(data []byte, num int) ([]byte) {
+	var resultData  []byte
 	if (p.curStatus == WaitForLength){
-		if (num >= 4){
-			p.needDataLength = 1
+		if (num + p.curHeadLength >= 4){
+			lengthArray := data[0:4]
+			p.needDataLength = binary.BigEndian.Uint32(lengthArray)
 			p.curStatus = WaitForData
+			p.curHeadLength = 0
+		}else
+		{
+			p.curHeadLength = num
 		}
 	}else{
 		if (p.curDataLength + num >= p.needDataLength){
-			
+			p.curData = append(p.curData, data[0:p.needDataLength])
+			p.curStatus = WaitForLength
+			resultData = p.curData
+		}else{
+			p.curData = append(p.curData, data[0:num])
+			p.curDataLength = p.curDataLength + num
 		}
 	}
+	return resultData
 }
+
+
+func appendData(ta)
 
 
 
